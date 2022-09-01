5afa264fecbac88a5a85382c3866aa0fc7d31358
Wbunker <thewillbunker@gmail.com>
2022-09-01 11:12:57 -0500

    Added lesson on __name__

---
+++ 002 What does if __name__ == "__main__": do/bad_greetings.py
@@ -0,0 +1,10 @@
+def spanish(name):
+    print(f"Hola {name}!")
+
+def english(name):
+    print(f"Hello {name}!")
+
+print(f"__name__ is {__name__}")
+
+spanish("Juan")
+english("Juan")
---
+++ 002 What does if __name__ == "__main__": do/greetings.py
@@ -0,0 +1,11 @@
+def spanish(name):
+    print(f"Hola {name}!")
+
+def english(name):
+    print(f"Hello {name}!")
+
+print(f"__name__ is {__name__}")
+
+if __name__ == '__main__':
+    spanish("Juan")
+    english("Juan")
